69d8b5917b879eec84fc6ee0f2774da39fb87f6d
ChingRu <contact@chingru.me>
2026-06-06 22:39:32 +0800

    self-host fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import "@fontsource/lato/300.css";
-import "@fontsource/lato/400.css";
-import "@fontsource/lato/700.css";
-import "@fontsource-variable/noto-sans-jp";
-import "@fontsource-variable/noto-sans-tc";
-import "@fontsource-variable/noto-sans-sc";
-
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
 
